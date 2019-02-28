019abd774bb8628e71bf123cb5372e6402b3b129
RobertHawkes <rhawkes02@qub.ac.uk>
2019-02-28 10:54:18 +0000

    "$commitMessage"

---
+++ alignFilesWithRepo.sh
@@ -0,0 +1,4 @@
+echo 'Resetting files to remote repo state'
+git reset --hard HEAD~1
+echo 'Reset files'
+sleep 3
---
+++ pushToRepo.sh
@@ -0,0 +1,4 @@
+echo 'Pushing to repository'
+git push
+echo 'Pushed to repo'
+sleep 3
