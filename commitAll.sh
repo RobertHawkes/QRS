f090d57f87d8b970cb51069edde79dd29bd8f9e8
RobertHawkes <rhawkes02@qub.ac.uk>
2019-02-28 10:32:34 +0000

    "$commitMessage"

--- commitAll.sh
+++ commitAll.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 git checkout master
 git add *
-echo Please enter commit message
+echo 'Please enter commit message'
 read commitMessage
-git commit -am commitMessage
-echo Commited to branch master with commit message: commitMessage
+git commit -am '"$commitMessage"'
+echo 'Commited to branch master with commit message: "$commitMessage"'
 read
