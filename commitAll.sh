84f4e5f0b03fa6a1bf7eeadb809571b80c56a791
RobertHawkes <rhawkes02@qub.ac.uk>
2019-02-28 11:27:20 +0000

    Updating commit script

--- commitAll.sh
+++ commitAll.sh
@@ -3,6 +3,6 @@ git checkout master
 git add *
 echo 'Please enter a commit message:'
 read commitMessage
-git commit -m '\"$commitMessage\"'
+git commit -m "$commitMessage"
 echo Commited to branch master with commit message: $commitMessage
 sleep 3
