b0ed6625623cb9aa9aa42775eec4e82cbe89c25f
RobertHawkes <rhawkes02@qub.ac.uk>
2019-03-12 20:27:58 +0000

    Attempting a new algorithm

--- Line Following/LineFollowingV2/LineFollowingV2.ino
+++ Line Following/LineFollowingV2/LineFollowingV2.ino
@@ -6,13 +6,16 @@
 #define R_MOTOR_2 8
 #define L_MOTOR_1 9
 #define L_MOTOR_2 11
-#define LINE_FOLLOW_A A1
-#define LINE_FOLLOW_B A2
-#define LINE_FOLLOW_C A3
+#define LINE_FOLLOWING_A A1
+#define LINE_FOLLOWING_B A2
+#define LINE_FOLLOWING_C A3
+#define MOTOR_SPEED_OFFSET 2
+boolean foundLine = true;
+int amountOfSearches = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.being(9600);
+  Serial.begin(9600);
   pinMode(L_MOTOR_ENABLE, OUTPUT);
   pinMode(R_MOTOR_ENABLE, OUTPUT);
   pinMode(L_MOTOR_1, OUTPUT);
@@ -26,27 +29,66 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int valueLeft = analogRead(LINE_FOLLOW_A);
-  int valueCenter = analogRead(LINE_FOLLOW_B);
-  int valueRight = analogRead(LINE_FOLLOW_C);
-
+  int valueLeft = analogRead(LINE_FOLLOWING_A);
+  int valueCenter = analogRead(LINE_FOLLOWING_B);
+  int valueRight = analogRead(LINE_FOLLOWING_C);
 
   //If Left has lost the line, swivel to the right
   //If right has lost the line, swivel to the left
   //If center is on the line, go forward
-  boolean foundLine = true;
+  long noOfRecords = 0;
+  long totalValueCenter += analogRead(LINE_FOLLOWING_B);
   
-  while(foundLine) {
-    if(valueCenter > [Higher Detection Limit]) {
+  if (foundLine) {
+    //Serial.println("in the foundLine loop ---"+(String)valueCenter);
+    if (valueCenter < 800) {
       foundLine = false;
+
     }
     else {
-      goForward();
+      driveLeftMotor(MIN_MOTOR_SPEED);
+      driveRightMotor(MIN_MOTOR_SPEED + MOTOR_SPEED_OFFSET);
+
     }
   }
 
-  while(!foundLine) {
-    
+  if (!foundLine) {
+    // Serial.println("in the !foundLine loop ");
+    driveLeftMotor(0);
+    driveRightMotor(0);
+    delay(500);
+    int maxLeft = 600;
+    int currentLeftDelay = 0;
+    while (currentLeftDelay < maxLeft) {
+      driveLeftMotor(-MIN_MOTOR_SPEED);
+      driveRightMotor(MIN_MOTOR_SPEED + MOTOR_SPEED_OFFSET);
+      delay(20);
+      currentLeftDelay += 20;
+      if (analogRead(LINE_FOLLOWING_B) > 800) {
+        foundLine = true;
+        break;
+      }
+      driveLeftMotor(0);
+      driveRightMotor(0);
+      delay(30);
+    }
+
+    int maxRight = 950;
+    int currentRightDelay = 0;
+    while (currentRightDelay < maxRight) {
+      driveLeftMotor(MIN_MOTOR_SPEED);
+      driveRightMotor(-MIN_MOTOR_SPEED - MOTOR_SPEED_OFFSET);
+      delay(20);
+      currentRightDelay += 20;
+      if (analogRead(LINE_FOLLOWING_B) > 800) {
+        foundLine = true;
+        break;
+      }
+      driveLeftMotor(0);
+      driveRightMotor(0);
+      delay(30);
+    }
+
     //Swivel to the left
     //Have we found the line yet?
     //If yes, set foundLine to true and exit loop
@@ -58,6 +100,7 @@ void loop() {
 
 void driveLeftMotor(int speed)
 {
+  speed = -speed;
   if (abs(speed) < MIN_MOTOR_SPEED) {
     analogWrite(L_MOTOR_ENABLE, speed);
     digitalWrite(L_MOTOR_1, LOW);
@@ -78,6 +121,7 @@ void driveLeftMotor(int speed)
 
 void driveRightMotor(int speed)
 {
+  speed = -speed;
   if (abs(speed) < MIN_MOTOR_SPEED) {
     analogWrite(R_MOTOR_ENABLE, speed);
     digitalWrite(R_MOTOR_1, LOW);
